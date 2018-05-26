94160fed530f24160cfea84f15fb6d0bb5702b4f
Nick Pierson <nick@nickpierson.name>
2018-05-26 06:58:43 +0000

    add loading screen before user auth loads

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
-import { auth } from './firebase';
+import { WanderingCubes } from 'better-react-spinkit';
 
+import { auth } from './firebase';
 import Home from './Home/Home';
 import Profile from './Profile/Profile';
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Home} />
-    <PrivateRoute path="/profile" component={Profile} />
-  </Switch>
-);
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = { isLoading: true };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => this.setState({ isLoading: false }));
+  }
+
+  render() {
+    const { isLoading } = this.state;
+
+    if (isLoading) return <LoadingSpinner />;
+
+    return (
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <PrivateRoute path="/profile" component={Profile} />
+      </Switch>
+    );
+  }
+}
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
@@ -19,6 +38,12 @@ const PrivateRoute = ({ component: Component, ...rest }) => (
       auth.currentUser ? <Component {...props} /> : <Redirect to="/" />
     }
   />
+);
+
+const LoadingSpinner = () => (
+  <div className="loading">
+    <WanderingCubes size={30} />
+  </div>
 );
 
 export default App;
