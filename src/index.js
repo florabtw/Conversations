b24f3b7551a28840aa0ef5dbc54ec1ff3145f8b8
Nick Pierson <nick@nickpierson.name>
2018-05-27 00:06:32 +0000

    scroll to top on route change

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { withRouter, BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
 import './index.css';
 import './styles/Section.css';
 
+class ScrollToTop extends Component {
+  componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  render() {
+    return this.props.children;
+  }
+}
+
+const RestoreScroll = withRouter(ScrollToTop);
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <RestoreScroll>
+      <App />
+    </RestoreScroll>
   </BrowserRouter>,
   document.getElementById('root')
 );
